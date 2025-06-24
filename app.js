6a7a05aeac730110065ed178ecdb285415149285
hruthikabhavana <hruthikabhavana@gmail.com>
2025-06-24 12:22:40 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let a=Number(prompt("Enter 1st number:"));
+if (a%2==0){
+    alert("even");
+}
+else{
+    alert("odd");
+}
+let today = new Date()
+console.log(today);
+console.log(today.getDate());
+console.log(today.getMonth());
+console.log(today.getDay());
+console.log(today.getYear());
