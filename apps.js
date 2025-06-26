771ff3213910a46ec6799e0443f17cdf63724e3e
hruthikabhavana <hruthikabhavana@gmail.com>
2025-06-26 14:54:22 +0530

    initial

--- apps.js
+++ apps.js
@@ -1,43 +1,66 @@
-let a=Number(prompt("Enter 1st number:"));
-if (a%2==0){
-    alert("even");
-}
-else{
-    alert("odd");
-}
-let today = new Date()
-console.log(today);
-console.log(today.getDate());
-console.log(today.getMonth());
-console.log(today.getDay());
-console.log(today.getYear());
+// let a=Number(prompt("Enter 1st number:"));
+// if (a%2==0){
+//     alert("even");
+// }
+// else{
+//     aert("odd");
+// }
+// let today = new Date()
+// console.log(today);
+// console.log(today.getDate());
+// console.log(today.getMonth());
+// console.log(today.getDay());
+// console.log(today.getYear());
 
-function login(){
-    let username = document.getElementById("uname").value;
+// function login(){
+//     let username = document.getElementById("uname").value;
     
-    let password = document.getElementById("pwd").value;
-    if(username==="admin" && password==="admin"){
-        alert("success");
-    }
-    else{
-        alert("failed");
-    }
-}
-function getloc(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPos,showerror);
-    }else{
-        console.log("not supported");
+//     let password = document.getElementById("pwd").value;
+//     if(username==="admin" && password==="admin"){
+//         alert("success");
+//     }
+//     else{
+//         alert("failed");
+//     }
+// }
+// function getloc(){
+//     if(navigator.geolocation){
+//         navigator.geolocation.getCurrentPosition(showPos,showerror);
+//     }else{
+//         console.log("not supported");
 
-    }
-}
-function showPos(position){
-    let lat=position.coords.latitude;
-    let lon=position.coords.longitude;
-    document.getElementById("p1").innerText = " latitude = "+lat+" longitude ="+lon;
+//     }
+// }
+// function showPos(position){
+//     let lat=position.coords.latitude;
+//     let lon=position.coords.longitude;
+//     document.getElementById("p1").innerText = " latitude = "+lat+" longitude ="+lon;
+// }
+
+// function showerror(error){
+//     console.log(error);
+// }   
+
+// class Student {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() {
+//         console.log(this.name);
+//     }
+// }
+
+// const C = new Student("Bob", 28);
+// C.greet(); 
+
+function change() {
+    document.getElementById("p1").innerText = "hello";
+    document.querySelector(".h1").innerHTML += "<button>h1</button>";
 }
 
-function showerror(error){
-    console.log(error);
-}   
-
+function show() {
+    let uname = document.getElementById("uname").value;
+    document.getElementById("un").innerText = uname;
+}
--- function.js
+++ function.js
@@ -1,10 +1,10 @@
-function EvenOdd(){
-let a=10;
-if(a%2==0){
-    alert("even");
+// function EvenOdd(){
+// let a=10;
+// if(a%2==0){
+//     alert("even");
 
-}else{
-    alert("odd");
-}
-}
-EvenOdd();
+// }else{
+//     alert("odd");
+// }
+// }
+// EvenOdd();
